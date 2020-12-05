3e7289303b02ce1fbbccf6b27161b0aa2a425c5c
vanpt1114 <48898401+vanpt1114@users.noreply.github.com>
2020-12-05 10:07:08 +0700

    Update allow.go

--- config/allow.go
+++ config/allow.go
@@ -1,12 +1,6 @@
 package config
 
 var AllowProject = map[int]string{
-    1604: "C019QRNJ6DN",
-    1052: "C01AY72GHT2",
-    1714: "C01AY72GHT2", // sre/application.cluster, channel #sre
-    1750: "C01AY72GHT2",
-    1603: "C01FPJ7CHGQ",
-    1640: "C01FPJ7CHGQ",
     1038: "C019QRNJ6DN", // workloads develop, channel #new-test-alerts
     1067: "C019QRNJ6DN", // workloads staging, channel #new-test-alerts
     1068: "C019QRNJ6DN", // workloads production, channel #new-test-alerts
