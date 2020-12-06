db203caccdf2c6c7445487ed310d6f5b233633bf
Van Phan <van.pt@teko.vn>
2020-12-07 00:44:16 +0700

    Convert more markdown

--- internal/model/slack_message.go
+++ internal/model/slack_message.go
@@ -12,8 +12,20 @@ var Pattern = map[string]string{
     `\-\s\[\+\]`:   ":todo_done:",
 }
 
-var Capture = map[string]string{
-    `#{1,6}\s(?P<Title>[0-9a-zA-Z-\s]+)\n`: "",
+var Bold = map[string]string{
+    `\*\*\*([0-9a-zA-Z-./#: ]+)\*\*\*`: "*_", // Bold & Italic
+    `\*([0-9a-zA-Z-./#: ]+)\*`: "_", // Italic
+    `\*\*([0-9a-zA-Z-./#: ]+)\*\*`: "*", // Bold
+    `\_\_([0-9a-zA-Z-./#: ]+)\_\_`: "*", // Bold
+    `#{1,6}\s([0-9a-zA-Z-./#: ]+)`: "*", // Heading
+}
+
+func Reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
 }
 
 func Author(user User) (author Block) {
@@ -104,14 +116,24 @@ func SlackMarkDown(data string) string {
         re := regexp.MustCompile(k)
         data = re.ReplaceAllString(data, v)
     }
-    // Replace markdown Heading
-    for k, _ := range Capture {
+
+    // Replace markdown Bold, Italic
+    for k, v := range Bold {
         re := regexp.MustCompile(k)
         tmp := re.FindAllStringSubmatch(data, -1)
         for _, val := range tmp {
-            re := regexp.MustCompile(string(val[0]))
-            data = re.ReplaceAllString(data, "*" + string(val[1]) + "*\n")
+            re := regexp.MustCompile(regexp.QuoteMeta(val[0]))
+            data = re.ReplaceAllString(data, v + val[1] + Reverse(v))
         }
     }
+
+    // Replace markdown Hyperlink
+    re := regexp.MustCompile(`\[([\s0-9a-zA-Z-.]+)\]\((https?:\/\/[0-9a-zA-Z\-./#]+)\)`)
+	result := re.FindAllStringSubmatch(data, -1)
+	for _, val := range result {
+		re := regexp.MustCompile(regexp.QuoteMeta(val[0]))
+		hyperlink := fmt.Sprintf("<%s|%s>", val[2], val[1])
+		data = re.ReplaceAllString(data, hyperlink)
+	}
     return data
 }
