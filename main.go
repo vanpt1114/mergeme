2bb410b26cffe621ff95b186d0b8732a3c2bddf4
vanpt <van.phan1@be.com.vn>
2021-09-17 11:07:38 +0700

    Remove unused block

--- main.go
+++ main.go
@@ -10,42 +10,9 @@ import (
 
 var cfg *config.Config
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//    body, err := ioutil.ReadAll(r.Body)
-//    defer r.Body.Close()
-//    if err != nil {
-//        panic(err)
-//    }
-//    var tmp gitlab.MergeEvent
-//    err = json.Unmarshal(body, &tmp)
-//    if err != nil {
-//        panic(err)
-//    }
-//    channel := config.CheckAllow(tmp.Project.ID)
-//    if channel == "" {
-//       fmt.Printf("ProjectID %v is not allowed\n", tmp.Project.ID)
-//       return
-//    }
-//    if tmp.ObjectAttributes.WorkInProgress == true {
-//       fmt.Println("WIP is not allow")
-//       return
-//    }
-//    r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-//    decoder := json.NewDecoder(r.Body)
-//    var t gitlab.MergeEvent
-//    err = decoder.Decode(&t)
-//    if err != nil {
-//       return
-//    }
-//    svc := service.NewService(cfg)
-//    svc.HandleEvent(t)
-//}
-
-
 func health(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
 }
-
 
 func main() {
     cfg = config.Load()
